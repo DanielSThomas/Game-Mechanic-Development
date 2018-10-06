d111e5aa57cae02f7471239f1e805ad3f0612d60
AbroadGames <AbroadGames@users.noreply.github.com>
2018-10-06 14:36:25 +0100

    Created Basic Instanciate Script

---
+++ ParadoxMechanic/ParadoxMechanic/Assets/ParadoxMechanic/Code/ParadoxCreator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParadoxCreator : MonoBehaviour
+{
+
+    [SerializeField]private GameObject instanciateOB;
+    [SerializeField]private Transform instanciateLocation;
+    [SerializeField]private float lifeTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(1))
+        {
+            InstanciateObject();
+        }
+	}
+
+    private void InstanciateObject()
+    {
+        GameObject _copy = Instantiate(instanciateOB, instanciateLocation);
+        Destroy(_copy, lifeTime);
+    }
+}
