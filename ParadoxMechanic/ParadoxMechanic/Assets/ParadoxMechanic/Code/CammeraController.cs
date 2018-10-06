6d2fd7c198b7f92f3ace853fb0cad63ed7a7dd48
AbroadGames <AbroadGames@users.noreply.github.com>
2018-10-06 14:19:49 +0100

    Added Temp Translation Based Movement
    Took some old movement code from previous project as a temporary way to test the main mechanic which relys on some play movement.

---
+++ ParadoxMechanic/ParadoxMechanic/Assets/ParadoxMechanic/Code/CammeraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CammeraController : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+	// Use this for initialization
+	void Start ()
+    {
+        character = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
---
+++ ParadoxMechanic/ParadoxMechanic/Assets/ParadoxMechanic/Code/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]private float speed = 10.0F;
+
+  
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _verticalMovement = Input.GetAxis("Vertical") * speed;
+        float _horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        _verticalMovement *= Time.deltaTime;
+        _horizontalMovement *= Time.deltaTime;
+
+        transform.Translate(_horizontalMovement, 0, _verticalMovement);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+     
+     
+    }
+    
+
+
+}
