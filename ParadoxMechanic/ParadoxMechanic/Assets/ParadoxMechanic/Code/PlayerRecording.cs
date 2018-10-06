309a1e646a37aa1657df2199ef3aa6dda2798f26
AbroadGames <AbroadGames@users.noreply.github.com>
2018-10-06 15:01:37 +0100

    Created base code for recording the play position

---
+++ ParadoxMechanic/ParadoxMechanic/Assets/ParadoxMechanic/Code/PlayerRecording.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRecording : MonoBehaviour
+{
+
+    [SerializeField] private List<Vector3> recordedPoints;
+    [SerializeField] private Vector3 point;
+    [SerializeField] private float recordAccuracy;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine("RecordPoints");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator RecordPoints()
+    {
+        for(; ; )
+        {
+            point = this.transform.position;
+
+            recordedPoints.Add(point);
+
+            yield return new WaitForSeconds(recordAccuracy);
+
+            Debug.DrawRay(point, transform.up, Color.red, 10);
+        }
+        
+    }
+}
