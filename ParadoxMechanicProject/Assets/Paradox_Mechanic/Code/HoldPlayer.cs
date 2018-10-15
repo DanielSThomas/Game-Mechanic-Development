217a9a9979c3a41db6c5bccdfb57c41245871464
AbroadGames <AbroadGames@users.noreply.github.com>
2018-10-15 16:52:41 +0100

    Added Colliders to the top of clones to allow clones to act as platforms

---
+++ ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/HoldPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class HoldPlayer : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider col)
+        {
+            col.transform.parent = gameObject.transform;
+        }
+
+        private void OnTriggerExit(Collider col)
+        {
+            col.transform.parent = null;
+        }
+
+     
+    }
