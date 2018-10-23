fcd7cd2a44593a322e2e04799ab7e98aeefea284
AbroadGames <AbroadGames@users.noreply.github.com>
2018-10-23 14:24:08 +0100

    Removed Clone Life Time and Replaced it With A Max Clone Count

--- ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/ParadoxCreator.cs
+++ ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/ParadoxCreator.cs
@@ -8,21 +8,28 @@ public class ParadoxCreator : MonoBehaviour
     [SerializeField]private GameObject instanciateOB;
     [SerializeField]private Transform instanciateLocation;
     [SerializeField]private float lifeTime;
+
+    [SerializeField]private List<GameObject> allinstanciates;
+    [SerializeField]private int maxClones;
     
 	// Use this for initialization
 	void Start ()
     {
-
+        
         
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-       
-       
-            
-	}
+
+        if (allinstanciates.Count > maxClones)
+        {
+            Destroy(allinstanciates[0]);
+            allinstanciates.RemoveAt(0);
+        }
+
+    }
 
     public void CreateClone()
     {
@@ -32,6 +39,10 @@ public class ParadoxCreator : MonoBehaviour
     private void InstanciateObject()
     {
         GameObject _copy = Instantiate(instanciateOB, instanciateLocation);
-        Destroy(_copy, lifeTime);
+
+        allinstanciates.Add(_copy);
+
+       
+       //Destroy(_copy, lifeTime);
     }
 }
