703c8f6f9ee278953add3f562d7ad6c4d560c7c8
AbroadGames <AbroadGames@users.noreply.github.com>
2018-12-12 19:19:45 +0000

    Revert "Implimented dashing combos and crashing"
    This reverts commit f9164affff34074358650641213b68bf8daa4a2a.

--- ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/ThirdPersonMovement.cs
+++ ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/ThirdPersonMovement.cs
@@ -18,22 +18,11 @@ public class ThirdPersonMovement : MonoBehaviour
 
     [SerializeField] LayerMask raycastMask;
 
-    [SerializeField] Renderer Obrenderer;
-
     private Rigidbody rb;
 
     private Vector3 _lastvel;
-    private bool cooldown = false;
 
     private bool dashactive;
-    private bool crashed;
-    
-    [SerializeField]private bool chainActive = false;
-    [SerializeField]private float chain;
-    [SerializeField]private float chainWindow;
-
-    private float minDashWindow;
-    private float maxDashWindow;
 
     // Initialization----------------------------------------------------------
     void Start()
@@ -53,15 +42,12 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update------------------------------------------------------------------
     void Update()
     {
-        
 
         //Movement
         Movement();
 
         //Dash
         Dash();
-
-        ChainWindow();
 
         //Unlock Mouse
         if (Input.GetKeyDown("escape"))
@@ -77,7 +63,7 @@ public class ThirdPersonMovement : MonoBehaviour
     private void Movement()
     {
        // _lastvel = Vector3.zero;
-        float _zMovement = speed* 1.2f * Input.GetAxis("Vertical");
+        float _zMovement = speed * Input.GetAxis("Vertical");
         float _xMovement = speed * Input.GetAxis("Horizontal");
 
 
@@ -86,24 +72,23 @@ public class ThirdPersonMovement : MonoBehaviour
         
 
         //Movement
-        if (_velocity != Vector3.zero && dashactive == false && crashed == false)
+        if (_velocity != Vector3.zero && dashactive == false)
         {
             rb.MovePosition(rb.position + _velocity * Time.deltaTime);
             _lastvel = _velocity;
 
             
         }
-        else if(dashactive == true && crashed == false)
+        else if(dashactive == true)
         {
-            rb.MovePosition(rb.position + _lastvel * Time.deltaTime * speed * 1.5f);
+            rb.MovePosition(rb.position + _lastvel * Time.deltaTime * speed);
         }
-        else if(crashed == true)
-        {
-            rb.MovePosition(rb.position + Vector3.zero);
-        }
-       
+            
+
+
+
         //Rotation
-        if (_velocity != Vector3.zero && dashactive == false && crashed == false)
+        if (_velocity != Vector3.zero && dashactive == false)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_velocity), 0.5F);
         }
@@ -112,94 +97,24 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void Dash()
     {
-        if (Input.GetButtonDown("Jump") && cooldown == false && crashed == false && chainActive == false)
+        if (Input.GetButtonDown("Jump"))
         {
             //speed = 14;
             rb.useGravity = false;
-            Invoke("DashEnd", 0.15f);
-            Invoke("CooldownEnd", 1f);
+            Invoke("DashEnd", 0.2f);
             dashactive = true;
-            cooldown = true;
-            minDashWindow = 0.5f;
-            maxDashWindow = 0.8f;
-
-            chainWindow = 0;
-            chainActive = true;
         }
-        else if (Input.GetButtonDown("Jump") && chainActive == true && crashed == false && chainWindow > minDashWindow && chainWindow < maxDashWindow)
-        {
-            dashactive = true;
-            chainWindow = 0;
-            CancelInvoke();
-            if (minDashWindow > 0.2)
-            {
-                minDashWindow -= 0.03f;
-                maxDashWindow -= 0.03f;
-            }
-            Invoke("DashEnd", 0.15f);
-            Invoke("CooldownEnd", 1f);
-            
-        }
-        else if (Input.GetButtonDown("Jump") && crashed == false && chainActive == true && chainWindow < minDashWindow)
-        {
-            chainActive = false;          
-        }
-        
-
-
-
+         
     }
-
-    private void ChainWindow()
-    {   
-        if (chainActive == true)
-        {
-            chainWindow += Time.deltaTime;
-            if (chainWindow > 2)
-            {             
-                chainActive = false;
-            }
-        }
-        
-
-        
-    }
-
 
     private void DashEnd()
     {
-        
+        speed = 6;
         rb.useGravity = true;
         dashactive = false;
-      
     }
 
- 
-
-
-    private void CooldownEnd()
-    {
-        cooldown = false;
-    }
-
-    private void CrashEnd()
-    {
-        crashed = false;
-        Obrenderer.material.color = Color.gray;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (dashactive == true && crashed == false && other.tag == "wall")
-        {
-            //CancelInvoke();
-            chainActive = false;
-            crashed = true;
-            Invoke("CrashEnd", 0.3f);
-            Obrenderer.material.color = Color.red;
-        }
-    }
-
-  
+   
 
 
 }
