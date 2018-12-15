07cbac24d3cec54e9735b0ff125fd33f89a51586
AbroadGames <AbroadGames@users.noreply.github.com>
2018-12-15 15:28:18 +0000

    Changed rotation to snap rotation
    Credit to https://answers.unity.com/questions/1093355/rotate-object-smoothly-over-time-when-key-pressed.html for original code

--- ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/RotateCharacter.cs
+++ ParadoxMechanicProject/Assets/Paradox_Mechanic/Code/RotateCharacter.cs
@@ -2,24 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateCharacter : MonoBehaviour {
+public class RotateCharacter : MonoBehaviour
+{
 
-    private float rotatespeed = 2.0f;
-   
+    [SerializeField] private float smooth = 2.0f;
+    private Quaternion targetRotation;
+    private bool cooldown;
+
     // Use this for initialization
     void Start ()
     {
-		
-	}
+        targetRotation = transform.rotation;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float h = rotatespeed * Input.GetAxis("Mouse X");
-
+        if(Input.GetAxis("Mouse X") > 0.7f && cooldown == false)
+        {
+            targetRotation *= Quaternion.AngleAxis(45, Vector3.up);
+            cooldown = true;
+            Invoke("ResetCooldown", 0.3f);
+        }
        
+        if (Input.GetAxis("Mouse X") < -0.7 && cooldown == false)
+        {
+            targetRotation *= Quaternion.AngleAxis(-45, Vector3.up);
+            cooldown = true;
+            Invoke("ResetCooldown", 0.3f);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+    }
 
-        transform.Rotate(0, h, 0);
+    private void ResetCooldown()
+    {
+        cooldown = false;
     }
 
     
